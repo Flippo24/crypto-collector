8bee667502f1d12ebee23ca77f7ef5269e2b6a99
Flippo24 <Flippo24@gmx.de>
2018-08-05 14:39:06 +0200

    add Binance

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Exchanges = require('./src/exchanges');
 
 Exchanges.addExchange('gdax','BTC-USD');
-Exchanges.addExchange('gdax','BCH-USD');
-Exchanges.addExchange('gdax','ETH-USD');
-Exchanges.addExchange('gdax','LTC-USD');
+Exchanges.addExchange('binance','ETHBTC');
+// Exchanges.addExchange('gdax','ETH-USD');
+// Exchanges.addExchange('gdax','LTC-USD');
